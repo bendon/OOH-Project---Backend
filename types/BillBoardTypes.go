3751c83e15fe7a7a1b5b017a60869407ce8c2c1c
dunstana <dunstan@afyarekod.com>
2025-02-26 16:03:26 +0300

    update

--- types/BillBoardTypes.go
+++ types/BillBoardTypes.go
@@ -9,12 +9,12 @@ type CreateBillboardRequest struct {
 	Location        string    `json:"location" validate:"required"`
 	Latitude        float64   `json:"latitude" validate:"required,latitude"`
 	Longitude       float64   `json:"longitude" validate:"required,longitude"`
-	Accuracy        float64   `json:"accuracy" validate:"gte=0"`
-	Width           float64   `json:"width" validate:"required,gte=0"`
-	Height          float64   `json:"height" validate:"required,gte=0"`
+	Accuracy        float64   `json:"accuracy" `
+	Width           float64   `json:"width" `
+	Height          float64   `json:"height" `
 	Unit            string    `json:"unit" validate:"required,oneof=centimeters meters feet inches"`
 	Type            string    `json:"type"`
-	Price           float64   `json:"price" validate:"required,gte=0"`
+	Price           float64   `json:"price" `
 }
 
 type UpdateBillboardRequest struct {
