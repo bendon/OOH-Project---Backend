77d5062b45c6c329cd83ab16f6f026e1102ce4c3
dunstanee <dunstansafu@gmail.com>
2025-03-12 09:11:13 +0300

    update update

--- types/BillBoardTypes.go
+++ types/BillBoardTypes.go
@@ -42,12 +42,12 @@ type UpdateBillboardRequest struct {
 	Location        string            `json:"location" validate:"required"`
 	Latitude        float64           `json:"latitude" validate:"required,latitude"`
 	Longitude       float64           `json:"longitude" validate:"required,longitude"`
-	Accuracy        float64           `json:"accuracy" validate:"required"`
-	Width           float64           `json:"width" validate:"required,gt=0"`
-	Height          float64           `json:"height" validate:"required,gt=0"`
+	Accuracy        float64           `json:"accuracy"`
+	Width           float64           `json:"width"`
+	Height          float64           `json:"height"`
 	Unit            string            `json:"unit" validate:"required,oneof=centimeters meters feet inches"`
-	Type            string            `json:"type" validate:"required,oneof=digital static LED traditional"`
-	Price           float64           `json:"price" validate:"required,gt=0"`
+	Type            string            `json:"type"`
+	Price           float64           `json:"price"`
 	ObjectType      *string           `json:"objectType"`
 	Owner           *string           `json:"owner"`
 	OwnerContacts   *Int64ArrayJSONB  `json:"ownerContacts"`
